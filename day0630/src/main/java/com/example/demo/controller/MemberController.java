8304063d8341c144870d3b6791791a59120b3696
juhee ko <lala8524@gmail.com>
2022-06-30 09:52:45 +0900

    MemberController 완성 브렌치 병합완료

--- day0630/src/main/java/com/example/demo/controller/MemberController.java
+++ day0630/src/main/java/com/example/demo/controller/MemberController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MemberController {
 	
 	@GetMapping("/login")
+	@ResponseBody
 	public String login() {
 		return "로그인";
 	}
