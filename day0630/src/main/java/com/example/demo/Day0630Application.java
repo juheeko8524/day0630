72dd43ce02075924084dd07ec897c66cf70c5eec
juhee ko <lala8524@gmail.com>
2022-06-30 09:46:13 +0900

    MemberController완성[고주희]

--- day0630/src/main/java/com/example/demo/Day0630Application.java
+++ day0630/src/main/java/com/example/demo/Day0630Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Day0630Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Day0630Application.class, args);
 	}
---
+++ day0630/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MemberController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "로그인";
+	}
+	
+	@RequestMapping("/loginOK")
+	public String loginOK() {
+		return "redirect:/listBoard";
+	}
+}
